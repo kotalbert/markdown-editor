8f3af0dc8226655b37e8aefa36a6264ddc23a044
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-20 18:50:11 +0100

    Add stage 1

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,1 +1,14 @@
-# write your code her
+markdown_string = """\
+# John Lennon
+or ***John Winston Ono Lennon*** was one of *The Beatles*.
+Here are the songs he wrote I like the most:
+
+* Imagine
+* Norwegian Wood
+* Come Together
+* In My Life
+* ~~Hey Jude~~ (that was *McCartney*)
+"""
+
+if __name__ == '__main__':
+    print(markdown_string)
